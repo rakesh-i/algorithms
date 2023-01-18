d2c6add7f7e751903101ef06b6169ac480a9c1bb
rakesh-i <57840815+rakesh-i@users.noreply.github.com>
2023-01-18 12:31:06 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -7,9 +7,9 @@ using namespace std;
 int main()
 {
     int array[] = {5, 4, 3, 2, 1, 0}, t;
-    int n = sternel(array)
-    for(int i=0; i<sterlen; i++){
-        for(int j=0; j<sterlen-i; j++){
+    int n = sizeof(array)/sizeof(int)-1;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i; j++){
             if(array[j]>array[j+1]){
                 t = array[j];
                 array[j] = array[j+1];
@@ -18,7 +18,7 @@ int main()
         }
     }
 
-    for(int k=0; k<sterlen+1; k++){
+    for(int k=0; k<n+1; k++){
         printf("%d ", array[k]);
     }
     cout<<endl;
