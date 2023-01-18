8bdeb85ec19b501cc8f0a1528d4bc8aefbe90620
rakesh-i <57840815+rakesh-i@users.noreply.github.com>
2023-01-18 12:27:58 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,13 +1,15 @@
 #include<iostream>
 #include<math.h>
 #include<ctype.h>
+#include<cstring>
 using namespace std;
 
 int main()
 {
     int array[] = {5, 4, 3, 2, 1, 0}, t;
-    for(int i=0; i<5; i++){
-        for(int j=0; j<5; j++){
+    int n = sternel(array)
+    for(int i=0; i<sterlen; i++){
+        for(int j=0; j<sterlen-i; j++){
             if(array[j]>array[j+1]){
                 t = array[j];
                 array[j] = array[j+1];
@@ -16,7 +18,7 @@ int main()
         }
     }
 
-    for(int k=0; k<6; k++){
+    for(int k=0; k<sterlen+1; k++){
         printf("%d ", array[k]);
     }
     cout<<endl;
