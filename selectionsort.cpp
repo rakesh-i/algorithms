8c84f40af9fb94abc9b445e665dcf211a6c91ce7
rakesh-i <57840815+rakesh-i@users.noreply.github.com>
2023-02-01 11:59:25 +0530

    Update selectionsort.cpp

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,23 +1,37 @@
 #include<iostream>
 #include<math.h>
 #include<ctype.h>
-using namespace std;
+#include<cstring>
+#include <cfloat>
+#define SWAP(x, y, t)  ((t)=(x), (x)=(y), (y)=(t))
+
+void sort(int*, int);
 
 int main()
 {
-    int a[] = {5, 4, 3, 2, 1, 0, 16, -3, 8, -55, 5}, temp;
-    for(int i=10; i>=0; i--){
-        for(int j=0; j<i; j++){
-            if(a[j]>a[j+1]){
-                temp = a[j];
-                a[j] = a[j+1];
-                a[j+1] = temp;
+    int c;
+    printf("Enter the size of the array\n");
+    scanf("%d", &c);
+    int *a = (int*)calloc(c, sizeof(int));
+    for(int i=0; i<c; i++){
+        a[i] = rand()%100;
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    sort(a, c);
+    for(int j=0; j<c; j++){
+        printf("%d ", a[j]);
+    }
+    printf("\n");
+}
+
+void sort(int *a, int n){
+    int temp;
+    for(int i=0; i<n-1; i++){
+        for(int j=i+1; j<n; j++){
+            if(a[j]<=a[i]){
+                SWAP(a[j], a[i], temp);
             }
         }
     }
-
-    for(int k=0; k<10; k++){
-        cout<<a[k]<<" ";
-    }
-    cout<<endl;
 }
