4ef7fcf7d2b8822b95f94f5d5e1bd69ed5f5921e
rakesh-i <57840815+rakesh-i@users.noreply.github.com>
2023-02-01 12:38:35 +0530

    Update binarysearch.cpp

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,29 +1,67 @@
 #include<iostream>
 #include<math.h>
 #include<ctype.h>
-using namespace std;
+#include<cstring>
+#include <cfloat>
+#define SWAP(x, y, t)  ((t)=(x), (x)=(y), (y)=(t))
+#define COMPARE(x, y) (((x)<(y))?-1:((x)==(y))?0:1)
+
+void sort(int*, int);
+int binsearch(int*, int, int, int);
 
 int main()
 {
-    int a[] = {0, 1, 2, 3, 9, 21, 85, 91, 96, 99};
-    int first = 0;
-    int last = 9;
-    int mid = (last+first)/2;
-    int key = 1;
-    while(first<last){
-        if(a[mid]==key){
-            cout<<"key is found at "<<mid+1<<endl;
-            break;
-        }
-        if(key>a[mid]){
-            first = mid;
-            mid = (first+last)/2;
-        }
-        else{
-            last = mid;
-            mid = (first+last)/2;
+    int c;
+    printf("Enter the size of the array\n");
+    scanf("%d", &c);
+    int *a = (int*)calloc(c, sizeof(int));
+    for(int i=0; i<c; i++){
+        a[i] = rand()%100;
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    sort(a, c);
+    for(int j=0; j<c; j++){
+        printf("%d ", a[j]);
+    }
+    printf("\n");
+    int key;
+    printf("Enter the key to be searched\n ");
+    scanf("%d",&key);
+    int index = binsearch(a, key, 0, c-1);
+    if(index>=0){printf("key is found at %d ", index);}
+    else{
+        printf("Not found\n");
+    }
+}
+
+void sort(int *a, int n){
+    int temp;
+    for(int i=0; i<n-1; i++){
+        for(int j=i+1; j<n; j++){
+            if(a[j]<=a[i]){
+                SWAP(a[j], a[i], temp);
+            }
         }
     }
-    cout<<"Not found :("<<endl;
+}
 
+int binsearch(int *a, int key, int left, int right){
+    int middle, flag; 
+    while(left<=right){
+        middle = (left+right)/2;
+        flag = COMPARE(a[middle], key);
+        //printf("%d %d %d %d\n",flag, middle, right, left);
+        if(flag==0){
+            return middle;
+            break;
+        }
+        else if(flag==1){
+            right = middle-1;
+        }
+        else{
+            left = middle+1;
+        }
+    }
+    return -1;
 }
